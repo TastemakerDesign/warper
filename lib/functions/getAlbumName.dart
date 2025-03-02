ca9810e74df7e42732f2390b59080d37e0c8564a
Tastemaker <me@tastemaker.design>
2025-03-02 14:29:25 -0500

    Properly display album names with decimal points

--- lib/functions/getAlbumName.dart
+++ lib/functions/getAlbumName.dart
@@ -3,5 +3,5 @@ import "dart:io";
 import "package:path/path.dart" as p;
 
 String getAlbumName(FileSystemEntity fileSystemEntity) {
-  return p.basenameWithoutExtension(fileSystemEntity.parent.path);
+  return p.basename(fileSystemEntity.parent.path);
 }
